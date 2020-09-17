b4e513a455c9c0b554629549c3d504977bb72bd7
victor <victormpl1983@gmail.com>
2020-09-17 17:50:54 +0200

    nuevodia-1

--- dia-1/www/src.js
+++ dia-1/www/src.js
@@ -1,1 +1,31 @@
 'use strict'
+
+// const num_a = 8;
+// const num_b = 4;
+// const option = 3;
+
+// if(option === 0){
+//     console.log(num_a + num_b);
+// } else if (option === 1){
+//     console.log(num_a - num_b);
+// } else if(option === 2){
+//     console.log(num_a * num_b);
+// } else if(option === 3){
+//     if(num_b === 0){
+//         console.log('no es posible dividir entre 0');
+//     } else {
+//         console.log(num_a / num_b);
+//     }
+// } else if(option === 4){
+//     console.log(num_a ** num_b);
+/*  }else {
+    console.log('no es posible');
+ } */
+/* 
+ for(let i = 0;i <= 100;i += 10){
+     console.log(i);
+ } */
+
+ for(let i = 100; i >=0; i -= 25){
+     console.log(i);
+ }
