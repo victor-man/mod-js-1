87b022e3e270fcef3def5cbf086144dabb315888
victor <victormpl1983@gmail.com>
2020-09-29 21:30:29 +0200

    async

--- dia-2/www/js/ejerciciogit.js
+++ dia-2/www/js/ejerciciogit.js
@@ -119,3 +119,111 @@ async function searchFilms(words, page){
     const result = await searchFilms(myWord, myPage);
     console.log(result);
 })(); */
+
+/*  const url = 'https://www.el-tiempo.net/api/json/v2/provincias'
+
+
+async function getProvince(community) {
+
+    // Obtenemos el response.
+    const response = await fetch(`https://www.el-tiempo.net/api/json/v2/provincias`);
+    
+    // Transformamos el response en un objeto que JavaScript pueda interpretar.
+    const data = await response.json();
+
+    
+    const myProvinces = data.provincias.filter((province) => {
+        return province.COMUNIDAD_CIUDAD_AUTONOMA === `${community}`;
+    });
+    console.log(myProvinces);
+    return myProvinces;
+}
+
+getProvince('Galicia') */
+
+/* 
+//* / Llamamos a la función mediante una IIFE asíncrona.
+ (async() => {
+    const result = await getProvince();
+    console.log(result);
+})(); 
+ */ 
+
+
+//  const url = 'https://www.el-tiempo.net/api/json/v2/municipios'
+
+/*  async function getmunicipality(province) {
+
+    // Obtenemos el response.
+    const response = await fetch(`https://www.el-tiempo.net/api/json/v2/municipios`);
+    
+    // Transformamos el response en un objeto que JavaScript pueda interpretar.
+    const data = await response.json();
+    console.log(data);
+    
+    const myMunicipality = data.filter((municipality) => municipality.NOMBRE_PROVINCIA === `${province}`);
+    console.log(myMunicipality);
+    return myMunicipality;
+}
+
+getmunicipality('Pontevedra') */
+
+
+/* async function getmunicipality(province) {
+
+    // Obtenemos el response.
+    const response = await fetch(`https://www.el-tiempo.net/api/json/v2/municipios`);
+    
+    // Transformamos el response en un objeto que JavaScript pueda interpretar.
+    const data = await response.json();
+    console.log(data);
+    
+    const myMunicipality = data.filter((municipality) => municipality.NOMBRE_PROVINCIA === `${province}`);
+
+    const municipalityName = myMunicipality.map((municipality) => municipality.NOMBRE);
+    console.log(myMunicipality);
+    console.log(municipalityName);
+    return myMunicipality;
+}
+
+getmunicipality('A Coruña') */
+
+/* async function getmunicipality(province) {
+
+    // Obtenemos el response.
+    const response = await fetch(`https://www.el-tiempo.net/api/json/v2/municipios`);
+    
+    // Transformamos el response en un objeto que JavaScript pueda interpretar.
+    const data = await response.json();
+    console.log(data);
+    
+    const myMunicipality = data.filter((municipality) => municipality.NOMBRE_PROVINCIA === `${province}`);
+
+    const municipalityName = myMunicipality.map((municipality) => municipality.NOMBRE);
+
+    const personProvince = myMunicipality.reduce((acc, municipality )=> {
+        return acc += municipality.POBLACION_MUNI;
+    }, 0);
+
+    console.log(myMunicipality);
+    console.log(municipalityName);
+    console.log(personProvince);
+    return myMunicipality;
+}
+
+getmunicipality('A Coruña') */
+
+async function getmunicipality(){
+    const response = await fetch(`https://www.el-tiempo.net/api/json/v2/municipios`);
+    const data = await response.json();
+    console.log(data);
+    const myTown = data.filter((cod) => cod.NOMBRE === 'Vigo');
+    console.log(myTown);
+    const mycode = myTown.map((cod) => cod.CODPROV);
+    console.log(mycode);
+    const myid = myTown.map((cod) => cod.CODIGOINE.substring(0, 5));
+    console.log(myid);
+    
+} 
+
+getmunicipality()
